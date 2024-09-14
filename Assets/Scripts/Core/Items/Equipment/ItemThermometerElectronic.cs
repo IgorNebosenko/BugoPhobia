27a37b14f54046c1e4f780f3f456c9db4106637b
Igor Nebosenko <igor.nebosenko@gmail.com>
2024-09-14 13:13:17 +0300

    Partial maked thermometer

---
+++ Assets/Scripts/Core/Items/Equipment/ItemThermometerElectronic.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace ElectrumGames.Core.Items
+{
+    public class ItemThermometerElectronic : ItemInstanceBase, IGhostHuntingInteractable
+    {
+        [SerializeField] private TMP_Text thermometerText;
+        [SerializeField] private Light onLight;
+        
+        [SerializeField] private string textOff = "OFF";
+        [SerializeField] private string textOn = "SCAN";
+        [SerializeField] private string textOnFormat = "{0:0.0} C";
+        
+        private bool _isOn;
+        public bool IsElectricityOn { get; private set; }
+        
+        public override void OnMainInteraction()
+        {
+            _isOn = !_isOn;
+
+            if (_isOn)
+            {
+                thermometerText.text = textOn;
+                onLight.enabled = true;
+            }
+            else
+            {
+                thermometerText.text = textOff;
+                onLight.enabled = false;
+            }
+        }
+        
+        private void DisplayTemperature(float temperature)
+        {}
+
+        public override void OnAlternativeInteraction()
+        {
+        }
+        
+        public void OnGhostHuntInteractionEnter()
+        {
+        }
+
+        public void OnGhostHuntInteractionExit()
+        {
+        }
+    }
+}
