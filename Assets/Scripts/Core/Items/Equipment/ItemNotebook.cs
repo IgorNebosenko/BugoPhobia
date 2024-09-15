207220e50ed010d4883ab075379ac935698e53c4
Igor Nebosenko <igor.nebosenko@gmail.com>
2024-09-15 21:19:59 +0300

    Add medium notebook

---
+++ Assets/Scripts/Core/Items/Equipment/ItemNotebook.cs
@@ -0,0 +1,13 @@
+﻿namespace ElectrumGames.Core.Items
+{
+    public class ItemNotebook : ItemInstanceBase
+    {
+        public override void OnMainInteraction()
+        {
+        }
+
+        public override void OnAlternativeInteraction()
+        {
+        }
+    }
+}
