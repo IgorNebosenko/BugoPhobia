56977d10d8876e423b91ffe0f8178c122158fc9c
Igor Nebosenko <igor.nebosenko@gmail.com>
2024-09-20 16:11:16 +0300

    UI of singleplayer w/o logic

--- Assets/Scripts/UI/Boards/Views/SinglePlayerBoardView.cs
+++ Assets/Scripts/UI/Boards/Views/SinglePlayerBoardView.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using ElectrumGames.Core.Items;
+using ElectrumGames.GlobalEnums;
 using ElectrumGames.UI.Boards.Presenters;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Zenject;
 
 namespace ElectrumGames.UI.Boards.Views
 {
@@ -26,17 +30,37 @@ namespace ElectrumGames.UI.Boards.Views
         [Space]
         [SerializeField] private Button startButton;
         [Space]
-        [SerializeField] private TMP_Text inventoryText;
+        [SerializeField] private Transform inventoryObject;
         [Space]
         [SerializeField] private EquipmentItemComponent equipmentItemTemplate;
         [Space]
         [SerializeField] private SinglePlayerBoardPresenter presenter;
 
+        private List<EquipmentItemComponent> equipmentItems = new ();
+
+        private ItemsConfig _itemsConfig;
+        
         public override DisplayBoardsMenu DisplayBoardsMenu => DisplayBoardsMenu.SinglePlayer;
+
+        [Inject]
+        private void ItemsConfig(ItemsConfig itemsConfig)
+        {
+            _itemsConfig = itemsConfig;
+        }
 
         private void Start()
         {
             SetBalanceText(1067411);
+            
+            SetLevelText(134);
+            SetProgress(374, 7575);
+            
+            SetItemsList(new List<(ItemType type, int currentCount, int maxCount)>()
+            {
+                (ItemType.FlashLightSmall, 1, 4),
+                (ItemType.FlashLightMedium, 0, 4),
+                (ItemType.FlashLightBig, 0, 4)
+            });
         }
 
         public void SetBalanceText(decimal balance)
@@ -44,6 +68,38 @@ namespace ElectrumGames.UI.Boards.Views
             balance = Math.Floor(balance);
             
             balanceText.text = string.Format(balanceFormat, balance);
+        }
+
+        public void SetLevelText(int level)
+        {
+            levelText.text = string.Format(levelFormat, level);
+        }
+
+        public void SetProgress(float currentXp, float maxXp)
+        {
+            var percent = Mathf.RoundToInt(currentXp / maxXp * 100f);
+            
+            percentText.text = string.Format(percentFormat, percent);
+            percentImage.fillAmount = currentXp / maxXp;
+
+            experienceText.text = string.Format(experienceFormat, currentXp, maxXp);
+        }
+
+        public void SetItemsList(List<(ItemType type, int currentCount, int maxCount)> items)
+        {
+            foreach (var item in equipmentItems)
+            {
+                Destroy(item.gameObject);
+            }
+            
+            equipmentItems.Clear();
+
+            for (var i = 0; i < items.Count; i++)
+            {
+                var item = Instantiate(equipmentItemTemplate, inventoryObject);
+                item.OnInit(_itemsConfig, items[i].type, items[i].currentCount, items[i].maxCount);
+                equipmentItems.Add(item);
+            }
         }
     }
 }
