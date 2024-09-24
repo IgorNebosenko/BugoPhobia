3df42e0ad866b33835957926d3ee8ad752e42639
Igor Nebosenko <igor.nebosenko@gmail.com>
2024-09-24 18:46:06 +0300

    Add load demo scene

--- Assets/Scripts/EntryPoints/MissionSceneEntry.cs
+++ Assets/Scripts/EntryPoints/MissionSceneEntry.cs
@@ -1,9 +1,38 @@
-﻿using UnityEngine;
+﻿using ElectrumGames.Configs;
+using ElectrumGames.Core.Player;
+using ElectrumGames.MVP.Managers;
+using ElectrumGames.UI.Presenters;
+using UnityEngine;
+using Zenject;
 
 namespace ElectrumGames.EntryPoints
 {
     public class MissionSceneEntry : MonoBehaviour
     {
+        [SerializeField] protected Transform[] playerSpawnPoints;
         
+        private PlayersFactory _playersFactory;
+        
+        private ViewManager _viewManager;
+        private ConfigService _configService;
+        
+        [Inject]
+        private void Construct(PlayersFactory playersFactory, ViewManager viewManager, ConfigService configService)
+        {
+            _playersFactory = playersFactory;
+            
+            _viewManager = viewManager;
+            _configService = configService;
+        }
+        
+        private void Start()
+        {
+            _configService.FpsConfig = _configService.FpsConfig;
+            
+            _playersFactory.CreatePlayer(
+                true, playerSpawnPoints[0].position, playerSpawnPoints[0].rotation);
+
+            _viewManager.ShowView<InGamePresenter>();
+        }
     }
 }
--- Assets/Scripts/Injection/MissionSceneInstaller.cs
+++ Assets/Scripts/Injection/MissionSceneInstaller.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using ElectrumGames.Core.Environment;
+using ElectrumGames.Core.Items;
+using ElectrumGames.Core.Player;
 using ElectrumGames.UI;
+using ElectrumGames.UI.UiEvents;
 using UnityEngine;
 
 namespace ElectrumGames.Injection
@@ -9,6 +12,14 @@ namespace ElectrumGames.Injection
     {
         [Space]
         [SerializeField] private EnvironmentHandler environmentHandler;
+        [Space]
+        [SerializeField] private PlayersFactory playersFactory;
+        [SerializeField] private ItemsFactory itemsFactory;
+        [Space]
+        [SerializeField] private Camera injectedCamera;
+        [Space]
+        [SerializeField] private ItemMarkers itemMarkers;
+        
         
         protected override Assembly UiAssembly => typeof(UiAssemblyReference).Assembly;
 
@@ -18,7 +29,16 @@ namespace ElectrumGames.Injection
 
             Container.BindInstance(environmentHandler).AsSingle();
 
+            Container.BindInstance(playersFactory).AsSingle();
+            Container.BindInstance(itemsFactory).AsSingle();
+
+            Container.BindInstance(injectedCamera).AsSingle();
+
+            Container.BindInstance(itemMarkers).AsSingle();
+            
             Container.Bind<EnvironmentData>().AsSingle();
+            
+            Container.Bind<UiEventsHandler>().AsSingle().NonLazy();
         }
     }
 }
