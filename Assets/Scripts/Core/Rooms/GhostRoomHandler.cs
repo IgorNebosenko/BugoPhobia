a9105ea9139b3dee46a0feb3097c5960ea71f57a
IgorNebosenko <igor.nebosenko@gmail.com>
2024-09-25 15:29:26 +0300

    Add obstacle layer

--- Assets/Scripts/Core/Rooms/GhostRoomHandler.cs
+++ Assets/Scripts/Core/Rooms/GhostRoomHandler.cs
@@ -1,9 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ElectrumGames.Core.Rooms
 {
     public class GhostRoomHandler : MonoBehaviour
     {
-        
+        [SerializeField] private Transform[] activityPoints;
+        [SerializeField] private Transform[] sheltersPoints;
+
+        public IReadOnlyList<Transform> ActivityPoints => activityPoints;
+        public IReadOnlyList<Transform> SheltersPoints => sheltersPoints;
     }
 }
