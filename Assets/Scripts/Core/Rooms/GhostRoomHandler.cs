0fa8ac93381cb97049b03b3280171c2e3fad56ee
IgorNebosenko <igor.nebosenko@gmail.com>
2024-09-25 16:00:42 +0300

    v 0.0.3.1a(70)

--- Assets/Scripts/Core/Rooms/GhostRoomHandler.cs
+++ Assets/Scripts/Core/Rooms/GhostRoomHandler.cs
@@ -5,9 +5,12 @@ namespace ElectrumGames.Core.Rooms
 {
     public class GhostRoomHandler : MonoBehaviour
     {
+        [SerializeField] private Transform spawnPointAtRoom;
+        [Space]
         [SerializeField] private Transform[] activityPoints;
         [SerializeField] private Transform[] sheltersPoints;
 
+        public Transform SpawnPointAtRoom => spawnPointAtRoom;
         public IReadOnlyList<Transform> ActivityPoints => activityPoints;
         public IReadOnlyList<Transform> SheltersPoints => sheltersPoints;
     }
