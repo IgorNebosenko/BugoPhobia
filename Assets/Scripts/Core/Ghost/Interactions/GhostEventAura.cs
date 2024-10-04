4e5dce39e58ec3117fdfc4cb255ed57878db2020
IgorNebosenko <igor.nebosenko@gmail.com>
2024-10-04 12:33:42 +0300

    GhostEventAura

--- Assets/Scripts/Core/Ghost/Interactions/GhostEventAura.cs
+++ Assets/Scripts/Core/Ghost/Interactions/GhostEventAura.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using ElectrumGames.Core.Common;
 using ElectrumGames.Core.Player;
 using UnityEngine;
 
@@ -7,20 +8,38 @@ namespace ElectrumGames.Core.Ghost.Interactions
     public class GhostEventAura : MonoBehaviour
     {
         private List<IPlayer> _playersInAura = new();
-        
+        private List<IGhostHuntingInteractableStay> _ghostHuntingInteractableStay = new();
+        private List<IGhostHuntingInteractableExit> _ghostHuntingInteractableExit = new();
         
         public IReadOnlyList<IPlayer> PlayersInAura => _playersInAura;
+
+        public IReadOnlyList<IGhostHuntingInteractableStay> GhostHuntingInteractableStay =>
+            _ghostHuntingInteractableStay;
+
+        public IReadOnlyList<IGhostHuntingInteractableExit> GhostHuntingInteractableExit =>
+            _ghostHuntingInteractableExit;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.TryGetComponent<IPlayer>(out var player))
                 _playersInAura.Add(player);
+            if (other.TryGetComponent<IGhostHuntingInteractableStay>(out var ghostHuntingInteractableStay))
+                _ghostHuntingInteractableStay.Add(ghostHuntingInteractableStay);
         }
 
         private void OnTriggerExit(Collider other)
         {
             if (other.TryGetComponent<IPlayer>(out var player))
                 _playersInAura.Remove(player);
+            if (other.TryGetComponent<IGhostHuntingInteractableStay>(out var ghostHuntingInteractableStay))
+                _ghostHuntingInteractableStay.Remove(ghostHuntingInteractableStay);
+            if (other.TryGetComponent<IGhostHuntingInteractableExit>(out var ghostHuntingInteractableExit))
+                _ghostHuntingInteractableExit.Add(ghostHuntingInteractableExit);
+        }
+
+        public void ResetGhostInteractableExit()
+        {
+            _ghostHuntingInteractableExit.Clear();
         }
     }
 }
