bd37d903f609e9ab7a9547c9ade4e735ae0945ba
IgorNebosenko <igor.nebosenko@gmail.com>
2024-10-16 19:57:06 +0300

    Change Icon and set random weather at demo

--- Assets/Scripts/Core/Environment/DemoEnvironmentHandler.cs
+++ Assets/Scripts/Core/Environment/DemoEnvironmentHandler.cs
@@ -5,13 +5,13 @@ namespace ElectrumGames.Core.Environment
     public class DemoEnvironmentHandler : EnvironmentHandler
     {
         
-        protected override void SetParams()
-        {
-            Debug.Log("Weather sets explicit!");
-            WeatherData = weatherConfig.Config[6];
-            OutDoorTemperature = WeatherData.OutdoorTemperature;
-
-            SetEnvironmentOutdoor();
-        }
+        // protected override void SetParams()
+        // {
+        //     Debug.Log("Weather sets explicit!");
+        //     WeatherData = weatherConfig.Config[6];
+        //     OutDoorTemperature = WeatherData.OutdoorTemperature;
+        //
+        //     SetEnvironmentOutdoor();
+        // }
     }
 }
