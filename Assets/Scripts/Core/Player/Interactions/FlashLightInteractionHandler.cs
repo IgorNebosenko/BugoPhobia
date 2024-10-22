fda913b5671bd928d2a9b0a7a2f8b0fc50ce97f7
Igor Nebosenko <igor.nebosenko@gmail.com>
2024-10-22 19:51:31 +0300

    Add IsElectricityOn = true/false

--- Assets/Scripts/Core/Player/Interactions/FlashLightInteractionHandler.cs
+++ Assets/Scripts/Core/Player/Interactions/FlashLightInteractionHandler.cs
@@ -7,12 +7,16 @@ namespace Core.Player.Interactions
         [SerializeField] private Light smallLight;
         [SerializeField] private Light mediumLight;
         [SerializeField] private Light bigLight;
+        
+        public bool IsElectricityOn { get; private set; }
 
         public void EnableSmallLight()
         {
             smallLight.enabled = true;
             mediumLight.enabled = false;
             bigLight.enabled = false;
+
+            IsElectricityOn = true;
         }
         
         public void EnableMediumLight()
@@ -20,6 +24,8 @@ namespace Core.Player.Interactions
             smallLight.enabled = false;
             mediumLight.enabled = true;
             bigLight.enabled = false;
+            
+            IsElectricityOn = true;
         }
         
         public void EnableBigLight()
@@ -27,6 +33,8 @@ namespace Core.Player.Interactions
             smallLight.enabled = false;
             mediumLight.enabled = false;
             bigLight.enabled = true;
+            
+            IsElectricityOn = true;
         }
         
         public void DisableLight()
@@ -34,6 +42,8 @@ namespace Core.Player.Interactions
             smallLight.enabled = false;
             mediumLight.enabled = false;
             bigLight.enabled = false;
+            
+            IsElectricityOn = false;
         }
     }
 }
