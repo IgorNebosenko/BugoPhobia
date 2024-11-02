a45729836ca9d9c181221189e98fcb20d92f2e60
IgorNebosenko <igor.nebosenko@gmail.com>
2024-11-02 16:48:10 +0200

    Start first popup - debug popup

---
+++ Assets/Scripts/UI/Presenters/DebugPopupPresenter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using ElectrumGames.MVP;
+using ElectrumGames.MVP.Utils;
+using ElectrumGames.UI.Views;
+
+namespace ElectrumGames.UI.Presenters
+{
+    public class DebugPopupPresenter : PopupPresenterCoroutine<DebugPopup, PopupArgs, PopupResult>
+    {
+        public DebugPopupPresenter(DebugPopup view) : base(view)
+        {
+        }
+
+        public override IEnumerable<PopupResult> Init(PopupArgs args)
+        {
+            yield break;
+        }
+
+        public void OnSanityFullClicked()
+        {
+        }
+
+        public void OnSanityEmptyClicked()
+        {
+        }
+
+        public void OnAddSanityClicked()
+        {
+        }
+
+        public void OnRemoveSanityClicked()
+        {
+        }
+
+        public void ForceInteractionClicked()
+        {
+        }
+
+        public void ForceGhostEventClicked()
+        {
+        }
+
+        public void ForceHuntClicked()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Views/DebugPopup.cs
@@ -0,0 +1,11 @@
+﻿using ElectrumGames.MVP;
+using ElectrumGames.UI.Presenters;
+
+namespace ElectrumGames.UI.Views
+{
+    [AutoRegisterView("Views/Popups/DebugPopup")]
+    public class DebugPopup : View<DebugPopupPresenter>
+    {
+        
+    }
+}
