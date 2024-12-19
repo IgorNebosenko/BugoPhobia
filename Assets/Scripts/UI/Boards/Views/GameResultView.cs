33a9711d3854b20fdaf48dfb8bbf730874eae79d
IgorNebosenko <igor.nebosenko@gmail.com>
2024-12-19 16:56:03 +0200

    Add game result view UI

---
+++ Assets/Scripts/UI/Boards/Views/GameResultView.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace ElectrumGames.UI.Boards.Views
+{
+    public class GameResultView : BoardViewBase
+    {
+        [SerializeField] private Color activeColor;
+        [SerializeField] private Color unActiveColor;
+        [Space]
+        [SerializeField] private string elementPattern;
+        [SerializeField] private string separatorPattern;
+        [SerializeField] private string ghostTypePattern;
+        [Space]
+        [SerializeField] private Transform infoBlock;
+        [SerializeField] private TMP_Text infoBlockElement;
+        [Space]
+        [SerializeField] private TMP_Text ghostType;
+        [SerializeField] private Button backButton;
+
+        private List<TMP_Text> _infoElements;
+        
+        public override DisplayBoardsMenu DisplayBoardsMenu => DisplayBoardsMenu.HuntResult;
+
+        [Inject]
+        private void Construct()
+        {
+        }
+
+        private void Start()
+        {
+            _infoElements = new List<TMP_Text>();
+        }
+    }
+}
