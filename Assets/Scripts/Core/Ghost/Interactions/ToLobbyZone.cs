b1750fafdffe12b16e921b110be9b544adaa35f9
IgorNebosenko <igor.nebosenko@gmail.com>
2024-12-25 18:28:32 +0200

    Add to lobby zone

---
+++ Assets/Scripts/Core/Ghost/Interactions/ToLobbyZone.cs
@@ -0,0 +1,14 @@
+﻿using ElectrumGames.Core.Missions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ElectrumGames.Core.Ghost.Interactions
+{
+    public class ToLobbyZone : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            SceneManager.LoadSceneAsync((int)MissionMap.LobbyTier0);
+        }
+    }
+}
