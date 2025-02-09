b14321fca86c55cd2ede0a5b526418ed67bfd037
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-09 14:39:36 +0200

    Add sanity to lobby w/o logic

---
+++ Assets/Scripts/Core/Items/Equipment/ItemSanityRestorer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ElectrumGames.Core.Items
+{
+    public class ItemSanityRestorer : ItemInstanceBase
+    {
+        [SerializeField] private float restoreSanity;
+        
+        public override void OnMainInteraction()
+        {
+        }
+
+        public override void OnAlternativeInteraction()
+        {
+        }
+    }
+}
