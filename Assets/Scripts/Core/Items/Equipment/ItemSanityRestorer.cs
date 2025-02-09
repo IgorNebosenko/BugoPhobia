f5696eca43f4554f40bbd1e27c9f36560c8718aa
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-09 20:52:03 +0200

    Add destroy sanity restorer when it use

--- Assets/Scripts/Core/Items/Equipment/ItemSanityRestorer.cs
+++ Assets/Scripts/Core/Items/Equipment/ItemSanityRestorer.cs
@@ -5,9 +5,10 @@ namespace ElectrumGames.Core.Items
     public class ItemSanityRestorer : ItemInstanceBase
     {
         [SerializeField] private float restoreSanity;
-        
+
         public override void OnMainInteraction()
         {
+            Destroy(gameObject);
         }
 
         public override void OnAlternativeInteraction()
