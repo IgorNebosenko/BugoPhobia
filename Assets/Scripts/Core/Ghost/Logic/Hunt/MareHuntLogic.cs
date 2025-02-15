22a64de06404d67954b1fe7705f976b513d24d7e
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-15 20:57:49 +0200

    Set logic of hunt for Mare

--- Assets/Scripts/Core/Ghost/Logic/Hunt/MareHuntLogic.cs
+++ Assets/Scripts/Core/Ghost/Logic/Hunt/MareHuntLogic.cs
@@ -3,18 +3,35 @@ using ElectrumGames.Core.Ghost.Configs;
 using ElectrumGames.Core.Ghost.Controllers;
 using ElectrumGames.Core.Player;
 using ElectrumGames.Core.Rooms;
-using UnityEngine;
 
 namespace ElectrumGames.Core.Ghost.Logic.Hunt
 {
     public class MareHuntLogic : BaseHuntLogic
     {
+        private readonly GhostController _ghostController;
+        private readonly MissionPlayersHandler _missionPlayersHandler;
+        
+        private bool IsLightOnRoom =>
+            _ghostController.GetCurrentRoom().LightRoomHandler.IsLightOn &&
+            _ghostController.GetCurrentRoom().LightRoomHandler.IsElectricityOn;
+        
+        
         public MareHuntLogic(GhostController ghostController, GhostDifficultyData ghostDifficultyData,
             GhostActivityData activityData, MissionPlayersHandler missionPlayersHandler,
             GhostFlickConfig ghostFlickConfig, HuntPoints huntPoints) : base(ghostController, ghostDifficultyData,
             activityData, missionPlayersHandler, ghostFlickConfig, huntPoints)
         {
-            Debug.LogError("Mare must have different hunt logic on light");
+            _ghostController = ghostController;
+            _missionPlayersHandler = missionPlayersHandler;
+        }
+
+        protected override bool CanHuntBySanity()
+        {
+            if (IsLightOnRoom)
+                return _missionPlayersHandler.AverageSanity <= _ghostController.GhostEnvironmentHandler.GhostConstants
+                    .modifiedSanityStartHunting;
+            return _missionPlayersHandler.AverageSanity <=
+                   _ghostController.GhostEnvironmentHandler.GhostConstants.defaultSanityStartHunting;
         }
     }
 }
