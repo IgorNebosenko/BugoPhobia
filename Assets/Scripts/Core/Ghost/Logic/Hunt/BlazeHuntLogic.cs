02fc5f71433939e8983863fc2fa64e0f429ac4fd
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-15 19:19:53 +0200

    Blaze hunt logic

--- Assets/Scripts/Core/Ghost/Logic/Hunt/BlazeHuntLogic.cs
+++ Assets/Scripts/Core/Ghost/Logic/Hunt/BlazeHuntLogic.cs
@@ -1,21 +1,136 @@
-﻿using ElectrumGames.Configs;
+﻿using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using ElectrumGames.Configs;
 using ElectrumGames.Core.Ghost.Configs;
 using ElectrumGames.Core.Ghost.Controllers;
-using ElectrumGames.Core.Missions;
 using ElectrumGames.Core.Player;
 using ElectrumGames.Core.Rooms;
-using UnityEngine;
+using ElectrumGames.Extensions;
+using UniRx;
+using Debug = UnityEngine.Debug;
 
 namespace ElectrumGames.Core.Ghost.Logic.Hunt
 {
     public class BlazeHuntLogic : BaseHuntLogic
     {
+        private bool _isMovingStopped;
+
+        private const float StopTime = 0.5f;
+        
+        private const float MinHuntSpeed = 1.2f;
+        private const float LimitOfHuntSpeed = 2.25f;
+        
+        private readonly GhostController _ghostController;
+        private readonly GhostDifficultyData _ghostDifficultyData;
+        private readonly HuntPoints _huntPoints;
+        
         public BlazeHuntLogic(GhostController ghostController, GhostDifficultyData ghostDifficultyData, 
             GhostActivityData activityData, MissionPlayersHandler missionPlayersHandler, GhostFlickConfig flickConfig,
             HuntPoints huntPoints) : 
             base(ghostController, ghostDifficultyData, activityData, missionPlayersHandler, flickConfig, huntPoints)
         {
-            Debug.LogError("Blaze must to have non-standard hunt logic!");
+            _ghostController = ghostController;
+            _ghostDifficultyData = ghostDifficultyData;
+            _huntPoints = huntPoints;
+        }
+        
+        protected override async UniTask Hunt(CancellationToken token)
+        {
+            try
+            {
+                var isMoving = false;
+                const float distanceTolerance = 0.1f;
+
+                huntingSpeed = GhostConstants.defaultHuntingSpeed;
+
+                while (huntingSpeed >= MinHuntSpeed || _isMovingStopped)
+                {
+                    if (!isMoving)
+                    {
+                        isMoving = true;
+                        var randomPosition = _huntPoints.Positions.PickRandom().position;
+                        MoveToPoint(randomPosition, false);
+                    }
+                    else if (_ghostController.NavmeshRemainingDistance < distanceTolerance)
+                    {
+                        isMoving = false;
+                    }
+
+                    ThrowItemsOnHunt();
+                    TouchDoorsOnHunt();
+                    
+                    SpeedChange();
+
+                    CheckPlayerOnVisual();
+                    CheckPlayerOnElectronic();
+                    await UniTask.Delay(TimeSpan.FromMilliseconds(250), cancellationToken: token);
+                    CheckPlayerOnVisual();
+                    CheckPlayerOnElectronic();
+                    await UniTask.Delay(TimeSpan.FromMilliseconds(250), cancellationToken: token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Hunt was cancelled or other issue");
+            }
+            finally
+            {
+                StopHunt();
+            }
+        }
+
+        protected override void SpeedChange()
+        {
+            if (_ghostController.GhostHuntAura.PlayersInAura is {Count: > 0})
+            {
+                var isSeePlayer = false;
+                
+                for (var i = 0; i < _ghostController.GhostHuntAura.PlayersInAura.Count; i++)
+                {
+                    var directionToPlayer = _ghostController.GhostHuntAura.PlayersInAura[i].PlayerHead.position -
+                                            _ghostController.transform.position;
+                    if (IsSeePlayer(directionToPlayer)) //ToDo need to check is it chased player
+                    {
+                        isSeePlayer = true;
+                        break;
+                    }
+                }
+
+                if (isSeePlayer)
+                {
+                    if (huntingSpeed >= LimitOfHuntSpeed)
+                    {
+                        huntingSpeed = GhostConstants.defaultHuntingSpeed;
+                    }
+                    else
+                    {
+                        _isMovingStopped = true;
+                        huntingSpeed = 0f;
+                        Observable.Timer(TimeSpan.FromSeconds(StopTime))
+                            .Subscribe(_ =>
+                            {
+                                huntingSpeed = GhostConstants.defaultHuntingSpeed;
+                                _isMovingStopped = false;
+                            });
+                    }
+                }
+                else
+                {
+                    SpeedDown();
+                }
+            }
+            else
+            {
+                SpeedDown();
+            }
+        }
+
+        protected override void SpeedDown()
+        {
+            huntingSpeed -= _ghostDifficultyData.SpeedUpByIteration;
+            
+            _ghostController.SetSpeed(huntingSpeed);
         }
     }
 }
