81b43ed058a274023781c4fd32f142e4dccd2535
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-02-16 19:04:35 +0200

    CanHuntBySanity override for Bababuk

--- Assets/Scripts/Core/Ghost/Logic/Hunt/BabadukHuntLogic.cs
+++ Assets/Scripts/Core/Ghost/Logic/Hunt/BabadukHuntLogic.cs
@@ -1,20 +1,34 @@
-﻿using ElectrumGames.Configs;
+﻿using System.Linq;
+using ElectrumGames.Configs;
 using ElectrumGames.Core.Ghost.Configs;
 using ElectrumGames.Core.Ghost.Controllers;
 using ElectrumGames.Core.Player;
 using ElectrumGames.Core.Rooms;
-using UnityEngine;
+using ElectrumGames.GlobalEnums;
 
 namespace ElectrumGames.Core.Ghost.Logic.Hunt
 {
     public class BabadukHuntLogic : BaseHuntLogic
     {
+        private readonly GhostController _ghostController;
+        private readonly MissionPlayersHandler _missionPlayersHandler;
+        
         public BabadukHuntLogic(GhostController ghostController, GhostDifficultyData ghostDifficultyData,
             GhostActivityData activityData, MissionPlayersHandler missionPlayersHandler,
             GhostFlickConfig ghostFlickConfig, HuntPoints huntPoints) : base(ghostController, ghostDifficultyData,
             activityData, missionPlayersHandler, ghostFlickConfig, huntPoints)
         {
-            Debug.LogError("Babaduk must be stronger if deselected in journals!");
+            _ghostController = ghostController;
+            _missionPlayersHandler = missionPlayersHandler;
+        }
+
+        protected override bool CanHuntBySanity()
+        {
+            if(_ghostController.JournalManager.PlayerJournalInstance.SelectedGhost == GhostType.Babaduk ||
+                   _ghostController.JournalManager.OtherPlayersJournalInstances.Any(x => x.SelectedGhost == GhostType.Babaduk))
+                return _missionPlayersHandler.AverageSanity < _ghostController.GhostEnvironmentHandler.GhostConstants.modifiedSanityStartHunting;
+            
+            return _missionPlayersHandler.AverageSanity < _ghostController.GhostEnvironmentHandler.GhostConstants.defaultSanityStartHunting;
         }
     }
 }
