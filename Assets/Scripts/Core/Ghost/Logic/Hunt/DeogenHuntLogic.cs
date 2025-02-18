cc1a7c87413a4081ed6016769d325cb8fb0c6394
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-18 17:55:54 +0200

    Deogen hunt logic

--- Assets/Scripts/Core/Ghost/Logic/Hunt/DeogenHuntLogic.cs
+++ Assets/Scripts/Core/Ghost/Logic/Hunt/DeogenHuntLogic.cs
@@ -1,20 +1,29 @@
-﻿using ElectrumGames.Configs;
+﻿using System.Collections.Generic;
+using ElectrumGames.Configs;
 using ElectrumGames.Core.Ghost.Configs;
 using ElectrumGames.Core.Ghost.Controllers;
 using ElectrumGames.Core.Player;
 using ElectrumGames.Core.Rooms;
+using ElectrumGames.Extensions;
 using UnityEngine;
 
 namespace ElectrumGames.Core.Ghost.Logic.Hunt
 {
     public class DeogenHuntLogic : BaseHuntLogic
     {
+        private readonly IReadOnlyList<Vector3> _shelterPositions;
+        
         public DeogenHuntLogic(GhostController ghostController, GhostDifficultyData ghostDifficultyData,
             GhostActivityData activityData, MissionPlayersHandler missionPlayersHandler,
             GhostFlickConfig ghostFlickConfig, HuntPoints huntPoints) : base(ghostController, ghostDifficultyData,
             activityData, missionPlayersHandler, ghostFlickConfig, huntPoints)
         {
-            Debug.LogError("Deogen must have non-standard hunt logic!");
+            _shelterPositions = huntPoints.Shelters;
+        }
+
+        protected override Vector3 GetHuntMovePosition()
+        {
+            return _shelterPositions.PickRandom();
         }
     }
 }
--- Assets/Scripts/Core/Rooms/HuntPoints.cs
+++ Assets/Scripts/Core/Rooms/HuntPoints.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ElectrumGames.Core.Rooms
@@ -6,7 +7,9 @@ namespace ElectrumGames.Core.Rooms
     public class HuntPoints : MonoBehaviour
     {
         [SerializeField] private Transform[] huntPoints;
+        [SerializeField] private Transform[] shelters;
 
         public IReadOnlyList<Transform> Positions => huntPoints;
+        public IReadOnlyList<Vector3> Shelters => shelters.Select(x => x.position).ToList();
     }
 }
