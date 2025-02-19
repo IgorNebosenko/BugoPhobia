140b88e1b8163068c0d7b5160aa84fe31656d7c7
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-02-19 20:09:05 +0200

    CheatVariables

---
+++ Assets/Scripts/DebugElements/CheatVariables.cs
@@ -0,0 +1,10 @@
+using ElectrumGames.GlobalEnums;
+
+namespace ElectrumGames.DebugElements
+{
+    public static class CheatVariables
+    {
+        public static GhostType SelectedGhostType = GhostType.None;
+        public static int RoomId = -1;
+    }
+}
