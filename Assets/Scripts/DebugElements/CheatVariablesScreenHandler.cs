f49601a4f170d86c0a5006102ff39db8365cea7d
Ihor Nebosenko <igor.nebosenko@gmail.com>
2025-02-19 20:47:32 +0200

    Cheat menu release

---
+++ Assets/Scripts/DebugElements/CheatVariablesScreenHandler.cs
@@ -0,0 +1,18 @@
+﻿using ElectrumGames.GlobalEnums;
+using UnityEngine;
+
+namespace ElectrumGames.DebugElements.DebugElements
+{
+    public class CheatVariablesScreenHandler : MonoBehaviour
+    {
+        public void SetGhostType(int ghostType)
+        {
+            CheatVariables.SelectedGhostType = (GhostType)ghostType;
+        }
+
+        public void SetRoomId(int roomId)
+        {
+            CheatVariables.RoomId = roomId;
+        }
+    }
+}
