addb9a4a4175168aa2a71c01f5c72d072b946c4f
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-22 11:49:34 +0200

    Add ghost steps handler

---
+++ Assets/Scripts/Core/Ghost/GhostStepsHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ElectrumGames.Core.Ghost
+{
+    public class GhostStepsHandler : MonoBehaviour
+    {
+        public void Init()
+        {
+        }
+
+        public void Simulate(float speed, float time)
+        {
+            
+        }
+    }
+}
