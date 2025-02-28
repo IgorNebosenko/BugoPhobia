c2db4bc1abae4913dc525ee4778733b3404118ce
IgorNebosenko <igor.nebosenko@gmail.com>
2025-02-28 12:13:32 +0200

    Add outdoor temperature

--- Assets/Scripts/Core/Environment/LobbyEnvironmentHandler.cs
+++ Assets/Scripts/Core/Environment/LobbyEnvironmentHandler.cs
@@ -5,11 +5,14 @@ namespace ElectrumGames.Core.Environment
 {
     public class LobbyEnvironmentHandler : EnvironmentHandler
     {
+        [SerializeField] private float outDoorTemperature;
         protected override void SetParams()
         {
             Debug.LogWarning("Weather config for lobby must be read from SO");
             WeatherData = WeatherConfigData.CreateForLobby(125f, 220f);
             SetEnvironmentIndoor();
+
+            OutDoorTemperature = outDoorTemperature;
         }
 
         protected override void ApplyParams()
