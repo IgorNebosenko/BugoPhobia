638384d0f0bd488388915c3c7e2b2b6a863b15e4
IgorNebosenko <igor.nebosenko@gmail.com>
2025-03-03 16:11:47 +0200

    Add UV as glowstick

--- Assets/Scripts/Core/Items/Zones/Handlers/UvPrintHandler.cs
+++ Assets/Scripts/Core/Items/Zones/Handlers/UvPrintHandler.cs
@@ -1,9 +1,79 @@
-﻿using UnityEngine;
+﻿using System;
+using ElectrumGames.Extensions;
+using UniRx;
+using UnityEngine;
 
 namespace ElectrumGames.Core.Items.Zones.Handlers
 {
     public class UvPrintHandler : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject[] decals;
+        [SerializeField] private float vanishTime = 120f;
+        [SerializeField] private float glowDelay = 3f;
+
+        private GameObject _currentDecal;
+
+        private IDisposable _vanishProcess;
+
+        private float _timeWithoutLight;
+
+        private Material _material;
+        private static readonly int MainColor = Shader.PropertyToID("_MainColor");
+
+        public float Charge { get; private set; }
+
+        private void Awake()
+        {
+            for (var i = 0 ; i < decals.Length; i++)
+                decals[i].SetActive(false);
+            
+            //MakeRandomPrint();
+        }
+
+        private void Update()
+        {
+            if (_currentDecal.UnityNullCheck())
+                return;
+
+            _timeWithoutLight += Time.deltaTime;
+
+            if (_timeWithoutLight >= glowDelay && Charge > 0f)
+                Charge -= Time.deltaTime;
+            
+            _currentDecal.SetActive(Charge > 0);
+            
+            var color = _material.GetColor(MainColor);
+            color.a = Charge;
+            _material.SetColor(MainColor, color);
+        }
+
+        public void MakeRandomPrint()
+        {
+            _currentDecal = decals.PickRandom();
+            
+            _material = _currentDecal.GetComponent<MeshRenderer>().material;
+            
+            var color = _material.GetColor(MainColor);
+            color.a = 0f;
+            _material.SetColor(MainColor, color);
+            
+            _vanishProcess?.Dispose();
+
+            _vanishProcess = Observable.Timer(TimeSpan.FromSeconds(vanishTime)).
+                Subscribe((_) => _currentDecal = null);
+        }
+
+        public void ChargingProcess(float value)
+        {
+            if (_currentDecal.UnityNullCheck())
+                return;
+            
+            Charge += value;
+
+            if (Charge > 1f)
+                Charge = 1f;
+
+            _timeWithoutLight = 0f;
+        }
     }
 }
--- Assets/Scripts/Core/Items/Zones/UvZoneTrigger.cs
+++ Assets/Scripts/Core/Items/Zones/UvZoneTrigger.cs
@@ -5,11 +5,13 @@ namespace ElectrumGames.Core.Items.Zones
 {
     public class UvZoneTrigger : MonoBehaviour
     {
+        [SerializeField] private float forceUv = 0.5f;
+
         private void OnTriggerStay(Collider other)
         {
             if (other.TryGetComponent<UvPrintHandler>(out var uvPrintHandler))
             {
-                
+                uvPrintHandler.ChargingProcess(Time.deltaTime * forceUv);
             }
         }
     }
