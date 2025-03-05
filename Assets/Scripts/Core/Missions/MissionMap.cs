7fb177d1d5c63fb30f605eb00cc2c5b0f70dd1dd
IgorNebosenko <igor.nebosenko@gmail.com>
2025-03-05 17:39:21 +0200

    New system of scenes

--- Assets/Scripts/Core/Missions/MissionMap.cs
+++ Assets/Scripts/Core/Missions/MissionMap.cs
@@ -4,9 +4,22 @@
     {
         None = -1,
         Logo = 0,
-        LobbyTier0 = 1,
-        DemoHouse = 2,
-        SmallHouse = 3,
-        HauntedHouse = 4
+        SceneSelector = 1,
+        TutorialSelector = 2,
+        TutorialFull = 3,
+        TutorialShort = 4,
+        LobbyTier0 = 5,
+        LobbyTier1 = 6,
+        LobbyTier2 = 7,
+        LobbyTier3 = 8,
+        LobbyTier4 = 9,
+        LobbyTier5 = 10,
+        LobbyTier6 = 11,
+        LobbyTier7 = 12,
+        LobbyTier8 = 13,
+        LobbyTier9 = 14,
+        DemoHouse = 15,
+        Apartments = 16,
+        SmallHouse = 17
     }
 }
