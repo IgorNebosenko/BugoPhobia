c164fb662e618f794064d42c4aac20339e6d7fbf
IgorNebosenko <igor.nebosenko@gmail.com>
2025-03-05 11:28:15 +0200

    Add also one picture for notebook. Notebook logiv in process

--- Assets/Scripts/Core/Items/Equipment/ItemNotebook.cs
+++ Assets/Scripts/Core/Items/Equipment/ItemNotebook.cs
@@ -1,13 +1,30 @@
-﻿namespace ElectrumGames.Core.Items
+﻿using ElectrumGames.Extensions;
+using UnityEngine;
+
+namespace ElectrumGames.Core.Items
 {
     public class ItemNotebook : ItemInstanceBase
     {
+        [SerializeField] private GameObject[] pictures;
+
+        private bool _isPictureShows;
+        
         public override void OnMainInteraction()
         {
         }
 
         public override void OnAlternativeInteraction()
         {
+        }
+
+        public void ShowRandomPicture()
+        {
+            if (_isPictureShows)
+                return;
+
+            _isPictureShows = true;
+            
+            pictures.PickRandom().SetActive(true);
         }
     }
 }
