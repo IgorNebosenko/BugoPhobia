3a47038db49376b2ff077f3bb6d9c71b6f633abe
IgorNebosenko <igor.nebosenko@gmail.com>
2025-03-12 13:07:54 +0200

    Add emf zone and UV at tutorial

---
+++ Assets/Scripts/Core/Tutorial/TutorialUvHandlerInitializer.cs
@@ -0,0 +1,15 @@
+﻿using ElectrumGames.Core.Items.Zones.Handlers;
+using UnityEngine;
+
+namespace ElectrumGames.Core.Tutorial
+{
+    public class TutorialUvHandlerInitializer : MonoBehaviour
+    {
+        [SerializeField] private UvPrintHandler uvPrintHandler;
+
+        private void Start()
+        {
+            uvPrintHandler.MakeRandomPrint();
+        }
+    }
+}
